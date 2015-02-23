862843f1e9b7a27631dbc2e008fca44eaf88e43a
Ertan Ornek <ertan.ornek@diodostransportation.com>
2015-02-22 18:15:19 -0600

    final solution for programming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -9,13 +9,16 @@ makeCacheMatrix <- function(x = matrix()) {
   
   m <- NULL
   set <- function(y) {
+    
+    #create a function to define vector x and reset m (matrix)
     x <<- y
     m <<- NULL
   }
-  get <- function() x
-  setmatrix <- function(solve) m <<- solve
-  getmatrix <- function() m
-  list(set = set, get = get,
+  get <- function() x  # function that returns vector x
+  setmatrix <- function(solve) m <<- solve # solve the matrix and return the result
+  getmatrix <- function() m #returns the solution
+  #list of functions that are stored in makeCaceMatrix
+  list(set = set, get = get,  
        setmatrix = setmatrix,
        getmatrix = getmatrix)
   
@@ -32,15 +35,22 @@ cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   
   
-  m <- x$getmatrix()
+  # calls the function in makeCacheMatrix to return 
+  #the inverse of the matrix if already calcualted
+  m <- x$getmatrix() 
+  
+  # check if matrix alredy exists with the solution
   if(!is.null(m)) {
     message("getting cached matrix")
     return(m)
   }
   
-  
+  #return the matrix if not already solved
   mtrx <- x$get()
-  m <- solve(marx, ...)
+  #re-solve the matrix
+  m <- solve(mtrx, ...)
+  #set the solution so we don't have to calculate again.
   x$setmatrix(m)
+  #return the solution
   m
 }
